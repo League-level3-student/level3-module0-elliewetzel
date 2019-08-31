a1876a9c1a2d8b9124f68a806316a6f93dfb5515
League Student <league@iMac-23.attlocal.net>
2019-08-31 14:29:28 -0700

    ArrayMethods and Firework (IP)

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -1,28 +1,38 @@
 package _01_Simple_Array_Algorithms;
 
 public class _00_1D_Array_Methods {
+	
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-		for(int i = 0; i < values.length; i++) {
-			//wrong
+		int s = 0;
+		for(int i =0; i < values.length; i++) {
+			s += values[i];
 		}
-		return y;
+		return s;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		double a = 0;
+		for(int i = 0; i < values.length; i++) {
+			a += values[i];
+		}
+		a = a/values.length;
+		return a;
 	}
-	
+	//check
 	
 	//3. Complete the method so that it returns true if the integer
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
+		for(int i =0; i < array.length; i ++) {
+			if(array[i] == value) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -30,7 +40,11 @@ public class _00_1D_Array_Methods {
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-		
-		return 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == value) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
