b5b8ce56d0e42b50f6139dd5bf94e9bf109a6ba9
League Student <league@iMac-5.attlocal.net>
2019-09-08 16:32:52 -0700

    MoreArrayFun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,11 +1,20 @@
 package _03_More_Array_Fun;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MoreArrayFun {
-	static String[] m = new String[100];
+	static String[] m = new String[4];
+	static String a = "gato";
+	static String b = "perro";
+	static String c = "elefante";
+	static String d = "nadar";
 	//1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
+		m[0] = a;
+		m[1] = b;
+		m[2] = c;
+		m[3] = d;
 		Printstrings(m);
 		Reverse(m);
 		Skip(m);
@@ -24,7 +33,7 @@ public class MoreArrayFun {
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	public static void Reverse(String[] ph) {
-		for(int i = ph.length; i > 0; i--) {
+		for(int i = ph.length-1; i > 0; i--) {
 			System.out.println(ph[i]);
 		}
 	}
@@ -40,10 +49,15 @@ public class MoreArrayFun {
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
 	public static void Random(String[] q) {
+		ArrayList <String> kate = new ArrayList<String>();
 		Random r = new Random();
 		for(int i = 0; i < q.length; i++) {
-			System.out.println(q[i+r.nextInt(q.length)]);
+			kate.add(q[i]);
+		}
+		for(int i = kate.size()-1; i>0; i--) {
+			System.out.println(kate.remove(r.nextInt(kate.size())));
+		}
 		}
 		
-	}
+	
 }
